944d134cbec7ae8c15ecea3087466dde33e91893
Admin <admin.push@hackerrank.workspace>
2022-08-20 01:07:58 +0000

    Auto save changes before Switch to Local IDE
    Commit by HackerRank

--- src/components/customer-list/index.js
+++ src/components/customer-list/index.js
@@ -1,16 +1,34 @@
-import React  from "react";
+import React, { useState } from "react";
 import "./index.css";
 
 function CustomerList() {
+  const [customers, setCustomer] = useState([])
+  const [name, setName] = useState()
+
+  const handleCustomers = (e, value) => {
+    e.preventDefault();
+    setName('')
+    setCustomer({ ...customers, value })
+  }
+
   return (
     <div className="mt-75 layout-column justify-content-center align-items-center">
-      <section className="layout-row align-items-center justify-content-center">
-        <input type="text" className="large" placeholder="Name" data-testid="app-input"/>
-        <button type="submit" className="ml-30" data-testid="submit-button">Add Customer</button>
-      </section>
+      <form onSubmit={e => handleCustomers(e, name)} >
+        <section className="layout-row align-items-center justify-content-center">
+          <input type="text" className="large" placeholder="Name" data-testid="app-input" onChange={(e) => setName(e.target.value)} />
+          <button type="submit" className="ml-30" data-testid="submit-button">Add Customer</button>
+        </section>
+      </form>
 
       <ul className="styled mt-50" data-testid="customer-list">
-          <li className="slide-up-fade-in" data-testid="list-item1" key="list-item1" >Customer</li>
+        {customers && customers.map((customer, key) => {
+          return (
+            <li className="slide-up-fade-in" data-testid={`list-item${key}`} key={`list-item${key}`} >customer</li>
+          )
+        }
+        )
+        }
+
       </ul>
     </div>
   );
