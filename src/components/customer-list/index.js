a1391b6c741164b9b76beeb9feb72e3002bafcf2
Angelo Reis <angelo.desenvolvedor@gmail.com>
2022-08-19 22:32:58 -0300

    fix: add states to control form

--- src/components/customer-list/index.js
+++ src/components/customer-list/index.js
@@ -3,34 +3,51 @@ import "./index.css";
 
 function CustomerList() {
   const [customers, setCustomer] = useState([])
-  const [name, setName] = useState()
+  const [name, setName] = useState('')
 
-  const handleCustomers = (e, value) => {
+  const handleCustomers = (e) => {
     e.preventDefault();
+    name !== '' && setCustomer([...customers, name])
     setName('')
-    setCustomer({ ...customers, value })
+  }
+
+  const handleName = (e) => {
+    e.preventDefault();
+    setName(e.target.value)
   }
 
   return (
-    <div className="mt-75 layout-column justify-content-center align-items-center">
-      <form onSubmit={e => handleCustomers(e, name)} >
+    <form onSubmit={handleCustomers} >
+      <div className="mt-75 layout-column justify-content-center align-items-center">
         <section className="layout-row align-items-center justify-content-center">
-          <input type="text" className="large" placeholder="Name" data-testid="app-input" onChange={(e) => setName(e.target.value)} />
+          <input
+            type="text"
+            className="large"
+            placeholder="Name"
+            data-testid="app-input"
+            value={name}
+            onChange={(e) => handleName(e)} />
           <button type="submit" className="ml-30" data-testid="submit-button">Add Customer</button>
         </section>
-      </form>
 
-      <ul className="styled mt-50" data-testid="customer-list">
-        {customers && customers.map((customer, key) => {
-          return (
-            <li className="slide-up-fade-in" data-testid={`list-item${key}`} key={`list-item${key}`} >customer</li>
-          )
-        }
-        )
-        }
-
-      </ul>
-    </div>
+        {customers.length > 0 && (
+          <ul className="styled mt-50" data-testid="customer-list">
+            {customers.map((customer, key) => {
+              return (
+                <li
+                  className="slide-up-fade-in"
+                  data-testid={`list-item${key}`}
+                  key={`list-item${key}`}>
+                  {customer}
+                </li>
+              )
+            }
+            )
+            }
+          </ul>
+        )}
+      </div>
+    </form>
   );
 }
 
